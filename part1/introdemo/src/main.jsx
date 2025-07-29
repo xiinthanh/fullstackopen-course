2d7d30c31e5730cb99d2ad1350ed289e835d7c0c
xiinthanh <xiinthanh@gmail.com>
2025-07-29 22:11:27 +0700

    stateful component

--- part1/introdemo/src/main.jsx
+++ part1/introdemo/src/main.jsx
@@ -2,17 +2,6 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App.jsx';
 
-let counter = 0;
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const refresh = () => {
-  root.render(
-    <App counter={counter} />
-  )
-}
-
-setInterval(() => {
-  refresh();
-  counter += 1;
-}, 1000)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+);
