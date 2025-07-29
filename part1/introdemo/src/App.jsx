0e2d41cd54aa3a729def073e51f31a3e2aa71bbf
xiinthanh <xiinthanh@gmail.com>
2025-07-29 21:43:42 +0700

    manual re-rendering

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,27 +1,7 @@
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age;
-
+const App = (props) => {
+  const {counter} = props;
   return (
-    <div>
-      <p>
-        Hello {name}, you're {age} years old
-      </p>
-      <p>
-        So you were probably born in {bornYear()}
-      </p>
-    </div>
-  );
-}
-
-const App = () => {
-  const name = 'Peter';
-  const age = 10;
-  return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name={name} age={age} />
-      <Hello name="Maya" age={10+16} />
-    </>
+    <div>{counter}</div>
   )
 }
 
--- part1/introdemo/src/main.jsx
+++ part1/introdemo/src/main.jsx
@@ -2,6 +2,17 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-);
+let counter = 0;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const refresh = () => {
+  root.render(
+    <App counter={counter} />
+  )
+}
+
+setInterval(() => {
+  refresh();
+  counter += 1;
+}, 1000)
