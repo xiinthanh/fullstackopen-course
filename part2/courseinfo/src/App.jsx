81cd4d6b72a6fca6686b19b4b458e721d5f64550
xiinthanh <xiinthanh@gmail.com>
2025-08-01 15:08:29 +0700

    complete E2.5 courseinfo step 10

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,40 +1,4 @@
-const Header = (props) => <h2>{props.course}</h2>
-
-const Content = (props) => (
-  <div>
-    {props.parts.map((part) => (
-      <Part key={part.id} part={part} />
-    ))}
-  </div>
-)
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-)
-
-const Total = (props) => {
-  const total = props.parts.reduce((s, p) => {
-    console.log(s, p)
-    return s + p.exercises
-  }, 0)
-    return (
-    <p style={{ fontWeight: 'bold' }}>
-      total of {total} exercises
-    </p>
-  )
-}
-
-const Course = (props) => {
-  return (
-    <div>
-      <Header course={props.course.name} />
-      <Content parts={props.course.parts} />
-      <Total parts={props.course.parts} />
-    </div>
-  )
-}
+import Course from './components/Course';
 
 const App = () => {
   const courses = [
