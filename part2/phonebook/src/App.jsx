71f784456e71f0a54c1bef35cf74f6c0c113246c
xiinthanh <xiinthanh@gmail.com>
2025-12-08 08:46:03 +0700

    complete E2.12 - Phonebook step 7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -42,12 +42,15 @@ const App = () => {
     const personObject = {
       name: newName,
       number: newNumber,
-      id: persons.length + 1,
     };
 
-    setPersons(persons.concat(personObject));
-    setNewName('');
-    setNewNumber('');
+    axios
+      .post('http://localhost:3001/persons', personObject)
+      .then(response => {
+        setPersons(persons.concat(response.data));
+        setNewName('');
+        setNewNumber('');
+      })
   }
 
   const filteredPersons = persons.filter(person => 
